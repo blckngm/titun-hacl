f87aac59b15dbb16ddd2c0bce182726f4f3156a1
Yin Guanhao <sopium@mysterious.site>
2020-06-23 20:35:55 +0800

    Add benchmark of curve25519

---
+++ benches/curve25519.rs
@@ -0,0 +1,67 @@
+#![feature(test)]
+extern crate test;
+
+use evercrypt_sys::*;
+use test::Bencher;
+
+#[bench]
+fn bench_curve25519_scalarmult(b: &mut Bencher) {
+    let mut out = [0u8; 32];
+    let our_secret: [u8; 32] = [
+        165, 70, 227, 107, 240, 82, 124, 157, 59, 22, 21, 75, 130, 70, 94, 221, 98, 20, 76, 10,
+        193, 252, 90, 24, 80, 106, 34, 68, 186, 68, 154, 196,
+    ];
+    let their_public: [u8; 32] = [
+        230, 219, 104, 103, 88, 48, 48, 219, 53, 148, 193, 164, 36, 177, 95, 124, 114, 102, 36,
+        236, 38, 179, 53, 59, 16, 169, 3, 166, 208, 171, 28, 76,
+    ];
+    unsafe {
+        EverCrypt_AutoConfig2_init();
+    };
+
+    b.iter(|| unsafe {
+        EverCrypt_Curve25519_scalarmult(
+            out.as_mut_ptr(),
+            our_secret.as_ptr() as _,
+            their_public.as_ptr() as _,
+        );
+    })
+}
+
+#[bench]
+fn bench_curve25519_scalarmult_libsodium(b: &mut Bencher) {
+    let mut out = [0u8; 32];
+    let our_secret: [u8; 32] = [
+        165, 70, 227, 107, 240, 82, 124, 157, 59, 22, 21, 75, 130, 70, 94, 221, 98, 20, 76, 10,
+        193, 252, 90, 24, 80, 106, 34, 68, 186, 68, 154, 196,
+    ];
+    let their_public: [u8; 32] = [
+        230, 219, 104, 103, 88, 48, 48, 219, 53, 148, 193, 164, 36, 177, 95, 124, 114, 102, 36,
+        236, 38, 179, 53, 59, 16, 169, 3, 166, 208, 171, 28, 76,
+    ];
+    unsafe {
+        libsodium_sys::sodium_init();
+    };
+
+    b.iter(|| unsafe {
+        libsodium_sys::crypto_scalarmult(
+            out.as_mut_ptr(),
+            our_secret.as_ptr() as _,
+            their_public.as_ptr() as _,
+        );
+    })
+}
+
+#[bench]
+fn bench_curve25519_scalarmult_dalek(b: &mut Bencher) {
+    let our_secret: [u8; 32] = [
+        165, 70, 227, 107, 240, 82, 124, 157, 59, 22, 21, 75, 130, 70, 94, 221, 98, 20, 76, 10,
+        193, 252, 90, 24, 80, 106, 34, 68, 186, 68, 154, 196,
+    ];
+    let their_public: [u8; 32] = [
+        230, 219, 104, 103, 88, 48, 48, 219, 53, 148, 193, 164, 36, 177, 95, 124, 114, 102, 36,
+        236, 38, 179, 53, 59, 16, 169, 3, 166, 208, 171, 28, 76,
+    ];
+
+    b.iter(|| x25519_dalek::x25519(our_secret, their_public))
+}
